ac694771193915ed38d8231a0579b78dc8760151
YUKIKO IKEDA <applelele@gmail.com>
2020-04-09 00:31:08 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,33 @@
 class BankAccount
+#one instance of the class can transfer money to another instance through a Transfer class.
+  attr_accessor :balance, :status
+  attr_reader :name
 
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end
+
+  def deposit(amount)
+    self.balance += amount
+  end
+
+  def display_balance
+    "Your balance is $#{self.balance}."
+  end
+
+  def valid?
+    if self.status == "open" && self.balance > 0
+      true
+    else
+      false
+    end
+  end
+
+  def close_account
+    if self.status == "closed" || self.balance = 0 # not == 0
+      self.status = "closed"
+    end
+  end
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,46 @@
+require "pry"
 class Transfer
-  # your code here
+  # acts as a space for a transaction between two instances of the bank account class.
+  #Transfer instances should be able to reject a transfer
+  #if the accounts aren't valid or if the sender doesn't have the money
+
+  attr_accessor :sender, :receiver, :amount, :status
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+  end
+
+  def valid?
+    @sender.valid? && @receiver.valid? ? true : false
+    # self.sender = BankAccount.new # @sender=#<BankAccount:0x00 @name="">
+    # self.receiver = BankAccount.new
+    #<Transfer:0x00 @sender="", @receiver=""
+  end
+
+  def execute_transaction
+    # transfer = Transfer.new(@sender, @receiver, @amount)
+    # if transfer.valid? &&
+    if (@sender.valid? && @receiver.valid?) && @sender.balance > @amount && @status == "pending"#(each transfer can only happen once)
+      @sender.balance -= @amount
+      @receiver.balance += @amount
+      @status = "complete"
+    # elsif (!@sender.valid? || !@receiver.valid?) && (@sender.balance < @amount || @sender.status == "closed") && @status == "pending"
+    else
+      @status = "rejected"
+      "Transaction rejected. Please check your account balance."
+    end
+# binding.pry
+  end
+
+  def reverse_transfer
+    if @status == "completed" && (@sender.valid? && @receiver.valid?) && @sender.balance > @amount
+      @sender.balance += @amount
+      @receiver.balance -= @amount
+      @status = "reversed"
+    end
+    binding.pry
+  end
 end
